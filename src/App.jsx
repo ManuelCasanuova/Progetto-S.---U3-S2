5f74c6564325dbb83fd32ba7902e5b5743ab987c
ManuelCasanuova <manuelcasanuova@gmail.com>
2025-02-14 11:16:40 +0100

    Hero e Home

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import CustomNavBar from "./Components/CustomNavBar";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Footer from "./Components/Footer";
+import Home from "./Components/Home";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <BrowserRouter>
         <CustomNavBar />
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<Home />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,17 @@
+import { Col, Container, Row } from "react-bootstrap";
+
 const Hero = () => {
-  return <div></div>;
+  return (
+    <>
+      <Container fluid>
+        <Row className="justify-content-center mt-4">
+          <Col id="hero" xs={10} md={6} className="text-center">
+            <h1>Benvenuto in Dunder Miffflin Meteo Company</h1>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Hero;
